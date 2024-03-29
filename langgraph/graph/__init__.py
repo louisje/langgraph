95d1e15a257238b6889657fa1fb343670409f44e
Louis Jeng <louisje@gmail.com>
2024-03-29 02:44:24 +0000

    CompileGraph

--- langgraph/graph/__init__.py
+++ langgraph/graph/__init__.py
@@ -1,5 +1,5 @@
-from langgraph.graph.graph import END, Graph
+from langgraph.graph.graph import END, Graph, CompiledGraph
 from langgraph.graph.message import MessageGraph
 from langgraph.graph.state import StateGraph
 
-__all__ = ["END", "Graph", "StateGraph", "MessageGraph"]
+__all__ = ["END", "Graph", "StateGraph", "MessageGraph", "CompiledGraph"]
